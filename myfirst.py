b67e6856356b7092c361da0ee332196552b1d74e
dltmdcks07 <william070209@gmail.com>
2026-06-24 19:52:41 +0900

    Add files via upload

---
+++ myfirst.py
@@ -0,0 +1,285 @@
+##num1 = 10
+##print(num1, type(num1))
+
+##num2 = 3.14
+##print(num2, type(num2))
+
+##m_str = "안녕하세요 "
+##print(m_str, type(m_str))
+
+##print(1+1, type(1+1))
+
+##print(0.3+2.04, type(0.3+2.04))
+
+##print(1+2.3, type(1+2.3))
+
+##print(" \"이샘코딩학원\" ")
+##print(' "이샘코딩학원" ')
+##print(1,2,3,sep="+",end= "=6")
+
+##print(f">{'abc':5s}<")
+##print(f">{3.141592:6.2f}<")
+
+##print("48> %d %o" %(48,48), end = "")
+##print("48> %x %c" % (48,48))
+##print("%c %c" % (65,0*41))
+##print("%c %c" % (66,0*42))
+##print("%f %e" % (0.132, 0.132))
+##print("이샘%s학원" % ("코딩"))
+
+##num1 = 17
+##print(num1)
+##print("10진수 17= %d"%(num1))
+##print("10진수 17의 8진수 = %o" %(num1))
+##print("10진수 17의 16진수 = %x" %(num1))
+
+##num2= 97
+##print("%d %c" %(num2 , num2))
+
+##float1=1234.567
+##print(float1)
+##print("기본 출력 %f" %(float1))
+##print("지수표기법 %e" % (float1))
+##print("소수 둘째자리까지 %.2f" %(float1))
+
+##txt1="Hello"
+##txt2="World!"
+##print("기본 출력\n%s %s" % (txt1, txt2))
+##print("7칸 출력\n%7s %7s" % (txt1, txt2))
+
+##print("48 > {0} {0:o}" .format(48,48))
+##print("48 > {0:x} {1:c}" .format(48,48))
+##print("{0:c} {1:c}" .format(65, 0*41))
+##print("{0:c} {1:c}" .format(66, 0*42))
+##print("{0:f} {1:e}" . format(0.132, 0.132))
+##print("이샘{0}학원" .format("코딩"))
+
+##number= 48
+##hexA=0*41
+##coding="코딩"
+##print(f"48> {number} {number:o}")
+##print(f"48> {number:x} {number:c}")
+##print(f"{hexA} {hexA:c}")
+##print(f"{hexA} {hexA:c}")
+##print(f"{0.132} {0.132:e}")
+##print(f"이샘{coding}학원")
+
+##num1=17
+##print(num1)
+##print("10진수 17= {0}".format(num1))
+##print("10진수 17의 8진수 ={0:o}" .format(num1))
+##print("10진수 17의 16진수 ={0:x}" .format(num1))
+
+##num2 =98
+##print("{0} {1:c}" .format(num2, num2))
+
+##float1=1234.567
+##print(float1)
+##print("기본출력 {0:f}" .format(float1))
+##print("지수표기법 {0:e}" .format(float1))
+##print("소수 둘째자리까지 {0:.2f}" .format(float1))
+
+##txt1="Hello"
+##txt2="World!"
+##print("기본 출력 \n{0:s} {1:s}" .format(txt1,txt2))
+##print("7칸 출력\n{0:7s} {1:7s}?" .format(txt1,txt2))
+
+##num1=17
+##print(num1)
+##print(f'10진수 17={num1}')
+##print(f'10진수 17의 8진수= {num1:o}')
+##print(f'10진수 17의 16진수 = {num1:x}')
+
+##num2=100
+##print(f"{num2} {num2:c}")
+
+##float1=1234.567
+##print(float1)
+##print(f"기본출력 {float1:f}")
+##print(f"지수표기법 {float1:e}")
+##print(f"소수 둘째자리까지 {float1: .2f}")
+
+##txt1= "Hello"
+##txt2 = "World!"
+##print(f'기본 출력 \n{txt1:s}{txt2:s}')
+##print(f'7칸 출력 \n{txt1:7s} {txt2:7s}?')
+
+##r=5
+##pi=3.14
+##print('넓이는' ,r, "*", r, "=", r**2*pi) # Alt 3
+
+
+      
+##a= input("닉네임을 입력하세요 : ")
+##print("환영합니다",a,"님!")
+
+
+##print("이름\t나이\t지역")
+##print("윤인성\t25\t강서구")
+##print("윤아린\t24\t강서구")
+##print("구름\t3\t강서구")
+##print("\\ \\ \\ \\")
+##print("동해물과 백두산이 마르고 닳도록\n하느님이 보우하사 우리나라 만세\n무궁화 삼천리 화려강산 대한사람\n대한으로 길이 보전하세")
+##print("""동해물과 백두산이 마르고 닳도록
+##하느님이 보우하사 우리나라 만세
+##무궁화 삼천리 화려강산 대한사람
+##대한으로 길이 보전하세""")
+##print("""
+##동해물과 백두산이 마르고 닳도록
+##하느님이 보우하사 우리나라 만세
+##무궁화 삼천리 화려강산 대한사람
+##대한으로 길이 보전하세""")
+
+##str1='Hello string!'
+##str2 = "안녕 문자열!"
+##str3 = """안녕 변수!
+##안녕 문자열!
+##안녕 파이썬!"""
+
+##str1 = "파이썬"
+##str2 = "참 쉽다"
+##str3 = str1 + ", " + str2 + "!!"
+##print(str3)
+
+##str1= "파이썬 Lvup!"
+##str2 = str1 *3
+##print(str2)
+
+##str1 = "파이썬 문자열을 골라보자"
+##print(str1[0])
+##print(str1[4])
+##print(str1[9])
+##print(str1[2])
+##print(str1[6])
+##print(str1[12])
+##str2= "이샘코딩학원"
+##print(str2[1:3])
+##print(str2[2:])
+##print(str2[:2])
+##print(str2[::2])
+##print(str2[::-2])
+##word = "문자열과 인덱스"
+##print(word[0])
+##print(word[3])
+##print(word[5])
+##print(word[-1])
+##snack = "떡볶이 순대 튀김"
+##setmenu = snack[0] + snack [4] + snack[7]
+##print(setmenu)
+##word = "부분만 바꾸려고 하면 에러가 나요"
+##print(word)
+####word[0] = "수"
+##word = "새로 만들어 덮어쓰기는 가능"
+##print(word)
+##word = "슬라이싱으로 다양하게 문자를 잘라봅시다"
+##print(word[0:4])
+##print(word[7:9])
+##print(word[5:])
+##print(word[:12])
+##print(word[::3])
+##print(word[::-3])
+##song = "록도닳 고르마 이산두백 과물해동"
+##reverse = song[::-1]
+##print(reverse)
+##song = "동해물과 백두산이 마르고 닳도록"
+##part_song = song[:4]
+##print(part_song)
+##part_song = song[5:13]
+##print(part_song)
+##part_song = song[14:]
+##print(part_song)
+
+##ID = input("아이디: ")
+##PW = input("비밀번호: ")
+##print('당신의 아이디는 ' + "\""+ID +"\""+"이며, 비밀번호는 " + "\""+ PW + "\""+ "입니다.")
+
+
+##a = input("문자열을 입력하세요 : ")
+##print(a[1::2])
+##
+##
+##a= input("주민번호를 입력하세요(-포함) : ")
+##print(a[2:6])
+##
+##
+##a= "타파에벅서이썬스나짱만스"
+##print(a[11]+a[0]+a[3]+a[7]+a[2]+a[4],a[10]+a[8])
+
+##song = """동해물과 백두산이 마르고 닳도록
+##하느님이 보우하사 우리나라 만세.
+##무궁화 삼천리 화려 강산
+##대한사람, 대한으로 길이 보전하세."""
+######print(song[33:35])
+######print(song[67:69])
+##print(song[0:47], song[48] + song[68] + "\n" + song[51:])
+
+##str1= "파이썬, C언어, HTML/CSS, JAVA"
+##logic1 = "파이썬" in str1
+##print(logic1)
+##logic2= "C#" in str1
+##print(logic2)
+##poison = "이 파이썬 문자에는 독이 있어!"
+##print("독" in poison)
+##print("약" not in poison)
+##number = "12345678"
+##print("1" in number)
+##message = "다른 연산자끼리 섞어쓰는 것도 가능해."
+##print("연산자" in message[2:10])
+##print("연산자" not in "정말 다양한 방법으로" + "연산자를 쓸 수 있어")
+##string = "문자열 함수 & 문자열 함수"
+##print(string.find("자열"))
+##print(string.find("함수"))
+##print(string.rfind("자열"))
+##print(string.rfind("함수"))
+##string = "HelloPython"
+##number = "12345"
+##print(string.isalpha())
+##print(string.isnumeric())
+##print(number.isalpha())
+##print(number.isnumeric())
+##bab = "국밥 컵밥 초밥 김밥 비빔밥"
+##print("가장 앞에 있는 밥은 " + str(bab.find("밥"))+ "번째!")
+##print("가장 뒤에 있는 밥은 "+ str(bab.rfind("밥"))+ "번째!")
+##eng = "FunnyPython"
+##kor = "신나는파이썬"
+##blank = "B L A N K"
+##print(eng.isalpha())
+##print(kor.isalpha())
+##print(blank.isalpha())
+##num = "20240505"
+##date = "2024.05.05"
+##print(num.isnumeric())
+##print(date.isnumeric())
+
+
+##a= "강낭콩 옆 빈 콩깍지는 완두콩 깐 빈 콩깍지고 완두콩 옆 빈 콩깍지는 강낭콩 깐 빈 콩깍지다."
+##print("첫번째 완두콩은 ", a.find("완두콩"),"에 있습니다.")        
+##print("두번째 완두콩은 ", a.rfind("완두콩"), "에 있습니다.")
+##
+##print(a.find("완두콩 깐 빈 콩깍지"))
+##print(a.rfind("강낭콩 깐 빈 콩깍지"))
+##print(a[13:24])
+##print(a[39:50])
+##a= input("이메일을 입력해주세요 : ")
+##b= a.find("@")
+##print("이 이메일의 아이디는 ",a[:b])
+string = "문자열_함수는_참_다양해요"
+split_string = string.split("_")
+print(string)
+print(split_string)
+##string = string.replace("다양해", "멋져")
+##print(string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
